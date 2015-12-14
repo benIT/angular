fc2e758522edbb337eef81260e00b244eebc83a4
Benoit MASSON <benoit.masson@production.infra>
2015-12-14 11:56:05 +0100

    js & css

--- js/lib.js
+++ js/lib.js
@@ -2,3 +2,21 @@ function $(selector) {
 	return document.querySelectorAll(selector);
 }
 
+/**
+*Add a listener for the matching selector
+*/
+function on(elementSelector, event, behaviour) {
+	var elements = $(elementSelector) ;
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].addEventListener(event,behaviour);
+	}
+}
+/**
+Remove listener for the matching selector
+*/
+function off(elementSelector, event, behaviour) {
+	var elements = $(elementSelector) ;
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].removeEventListener(event, behaviour);
+	}
+}
--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ for (var i = 0; i < tyres.length; i++) {
 	console.log(tyres[i].innerText) ;
 };
 
-//
+on('event[action="add"]','click', function () {
+	console.log(new Date()) ;
+});
+
+off('event[action="add"]','click',function(){
+	console.log('inactive');
+}) ;
