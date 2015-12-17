030cc27237e5632a9404e96de53d149deccc3ebd
Adrien NIVON <adrien.nivon@production.infra>
2015-12-17 10:44:45 +0100

    ajout composant tyre

---
+++ components/tyres/js/state.js
@@ -0,0 +1,39 @@
+
+// Imports modules dependencies
+var imports = ['ngResource', 'filters', 'ui.router'];
+
+var tyres = angular.module('tyres', imports);
+
+// app configuration
+tyres.config(function ($stateProvider, $urlRouterProvider) {
+
+  	$stateProvider.state('tyres.subview', {
+  		url:'/:action/:key',
+	    views: {
+	    	'form': {
+	    		templateUrl: '/components/tyres/views/addTyre.tpl',
+	    		controller: 'ManageTyresController'  
+	    	}
+	    },	      
+	})
+  	.state('tyres.subviewadd', {
+	    views: {
+	    	'form': {
+	    		templateUrl: '/components/tyres/views/addTyre.tpl',
+	    		controller: 'ManageTyresController'  
+	    	}
+	    },	      
+	}).state('tyres',{
+  		url: '/tyres',
+        views : {        	
+        	'tyres': {
+        		templateUrl: '/components/tyres/views/tyres.tpl',        		
+        		controller: 'ListTyresController'
+        	},
+	    	'list@tyres': {
+	    		templateUrl: '/components/tyres/views/list.tpl',
+	    		controller: 'ListTyresController' 
+	    	 },
+        },         
+  	})
+});
--- js/controller.js
+++ js/controller.js
@@ -1,53 +1,5 @@
 'use strict';
 
-modis.controller('ListTyresController', ['$scope', '$state', 'TyresService', function ($scope, $state, TyresService) {
-	$scope.tyre = {pattern:{}};
-	$scope.readable = false;
-	$scope.displayForm = false;
-
-	$scope.getTyres = function(){
-		$scope.tyres = JSON.parse(sessionStorage.getItem("tyres"));
-		if(!$scope.tyres){
-			TyresService.query(function(response){
-				$scope.tyres = response[0];
-				sessionStorage.setItem('tyres', JSON.stringify($scope.tyres));
-			});
-		}
-	}
-
-	$scope.getTyres();
-
-	$scope.addTyre = function(){
-		if($scope.tyre.reference){
-			$scope.tyres[$scope.tyre.reference] = angular.copy($scope.tyre);
-			sessionStorage.setItem('tyres', JSON.stringify($scope.tyres));
-		}
-		$scope.displayForm = false;
-		$scope.tyre = {pattern:{}};
-		$state.go('home');
-	};
-}]);
-
-
-modis.controller('ManageTyresController', ['$scope',
-										   '$controller',
-										   '$stateParams', 
-										   '$state',
-										   'TyresService', function ($scope, 
-										   							 $controller,
-										   							 $stateParams,
-										   							 $state, 
-										   							 TyresService) {
-	// controller inheritance
-	$controller("ListTyresController", {$scope : $scope});
-
-	if($stateParams['action'] == "delete"){
-		delete $scope.tyres[$stateParams['key']];
-		sessionStorage.setItem('tyres', JSON.stringify($scope.tyres));
-		$state.go('home');
-	} else if($stateParams['action'] == "edit"){
-		$scope.readable = true;
-		$scope.tyre = angular.copy($scope.tyres[$stateParams['key']]);
-	}
-	
+modis.controller('Home', ['$scope', '$state', function ($scope, $state) {
+	$scope.WelcomeMessage = 'BIJOUR';
 }]);
